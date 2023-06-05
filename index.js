0df589094f9f5eb9b456eb93523006b741a30836
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2023-06-05 15:30:32 -0300

    aaa

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import {converter_binario} from "./js/binario.js"
 
-
-
 const btn_ = document.querySelector("#converter")
 const chat = document.querySelector("#chat")
 const resposta = document.querySelector("#resposta")
@@ -10,7 +8,7 @@ btn_.addEventListener("click", ()=>{
     let converter = converter_binario(Math.floor(chat.value))
     
     if(converter ==""){
-        alert("Digite apenas números inteiros !")
+        alert("Digite apenas números inteiros!")
     }
     resposta.innerHTML = converter
 
