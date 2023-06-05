bc0bc24e0eeba6cc34dc89beaa62254e396c6bba
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2023-06-05 15:53:59 -0300

    Update binario.js

--- js/binario.js
+++ js/binario.js
@@ -2,15 +2,20 @@ export function converter_binario(n){
     let valor = n
     let resto = 0 
     let lista = []
-    let resposta = 0 
-    while (valor >0){
-        resto = valor%2 
-        valor = Math.floor(valor/2)
-        lista.push(resto)
+    let resposta = 0
+    
+    if (valor == 0){
+        return "0" 
+    } 
+    else{
+        while (valor >0){
+            resto = valor%2 
+            valor = Math.floor(valor/2)
+            lista.push(resto)
+    
+        }  
+        resposta = lista.reverse()
+        return resposta.join("")
 
-    }  
-    resposta = lista.reverse()
-    return resposta.join("")
+    }
 }
-
-
